33997a6fb5a8e2c926eeb2c376ef847960b193fa
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-30 19:10:27 +0900

    feat: 자바스크립트 인라인 테스트

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -127,6 +127,16 @@ public class BasicController {
         return "basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model){
+        model.addAttribute("user", new User("UserA", 10));
+        addUsers(model);
+
+        return "basic/javascript";
+    }
+
+
+
     @Data
     static class User {
         private String username;
